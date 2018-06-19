1c8d04471e269f1d36c0fbc060a9fdb5b5ca5fa1
mpras55 <manjunath.bp@gmail.com>
2018-06-18 20:54:13 -0600

    Landing page

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Books from "./pages/Books";
-import Detail from "./pages/Detail";
+import Items from "./pages/Items";
+// import Books from "./pages/Books";
+// import Detail from "./pages/Detail";
 import NoMatch from "./pages/NoMatch";
 import Nav from "./components/Nav";
 
@@ -9,10 +10,11 @@ const App = () => (
   <Router>
     <div>
       <Nav />
+			
       <Switch>
-        <Route exact path="/" component={Books} />
-        <Route exact path="/books" component={Books} />
-        <Route exact path="/books/:id" component={Detail} />
+        <Route exact path="/" component={Items} />
+        {/* <Route exact path="/books" component={Books} />
+        <Route exact path="/books/:id" component={Detail} /> */}
         <Route component={NoMatch} />
       </Switch>
     </div>
---
+++ client/src/pages/Items/Items.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import listedItems from "./../../../src/listed.json";
+import ItemCard from "./../../components/ItemCard";
+import { Container } from './../../components/Grid/Container';
+import "./Items.css";
+class Items extends Component {
+
+	state = {
+		listedItems
+	};
+
+
+	render() {
+		return (
+			<span className="App">
+			<h1> Listed Items </h1>
+					{this.state.listedItems.map(listed => (
+						<ItemCard
+							id={listed.id}
+							key={listed.id}
+							name={listed.name}
+							image={listed.image}
+							value={listed.value}
+							location={listed.location}
+						/>
+					))}
+					{/* <div className="clear-fix">.</div> */}
+			</span>
+		);
+	}
+}
+
+export default Items;
