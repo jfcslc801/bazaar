3c3a842c9ca5de97916f92f5fde62c815b0b87b1
ctrlz <pehisaac@gmail.com>
2018-06-21 17:33:52 -0600

    Blended my changes with Jaime's Modal

--- client/src/components/Modal/Modal.js
+++ client/src/components/Modal/Modal.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { render } from "react-dom";
 import { Modal, Button } from 'react-materialize';
 
 const styles = {
--- client/src/pages/Items/Items.js
+++ client/src/pages/Items/Items.js
@@ -1,39 +1,7 @@
 import React, { Component } from 'react';
 import listedItems from "./../../../src/listed.json";
 import ItemCard from "./../../components/ItemCard";
-import { Table1, TableItem } from './../../components/BarterWindow';
-
 import "./Items.css";
-
-// This is a cheap fix, using this as a psuedo database for the time being until we have data stored in a database
-let dbOffer = [{
-	id: 1,
-	date: "09/09/09",
-	offer: "iPhone SE 64GB",
-	itemDetails: "It is a great condition phone, AT&T Network",
-	user: "Izzlenizzle"
-},
-{
-	id: 2,
-	date: "09/09/09",
-	offer: "iPhone SE 64GB",
-	itemDetails: "It is a great condition phone, AT&T Network",
-	user: "Izzlenizzle"
-},
-{
-	id: 3,
-	date: "09/09/09",
-	offer: "iPhone SE 64GB",
-	itemDetails: "It is a great condition phone, AT&T Network",
-	user: "Izzlenizzle"
-},
-{
-	id: 4,
-	date: "09/09/09",
-	offer: "iPhone SE 64GB",
-	itemDetails: "It is a great condition phone, AT&T Network",
-	user: "Izzlenizzle"
-}]
 
 class Items extends Component {
 
@@ -41,26 +9,10 @@ class Items extends Component {
 		listedItems
 	};
 
-
 	render() {
 		return (
 			<span className="App">
-
 				<h1> Listed Items </h1>
-
-
-				<Table1>
-				{dbOffer.map(offer => (
-					<TableItem
-					key={offer.id}
-					user={offer.user}
-					date={offer.date}
-					offer={offer.offer}
-					
-					/>
-				))}
-				</Table1>
-
 				{this.state.listedItems.map(listed => (
 					<ItemCard
 						id={listed.id}
@@ -77,4 +29,3 @@ class Items extends Component {
 }
 
 export default Items;
-
