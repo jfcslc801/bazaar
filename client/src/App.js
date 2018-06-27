bc9175f973653b7d06358d50df5ffd588d65685e
jmcam80 <jmcam80@gmail.com>
2018-06-26 18:38:36 -0600

    updated all vatiations of book, books, Book or Books

---
+++ client/src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Items from "./pages/Items";
+import NoMatch from "./pages/NoMatch";
+import Nav from "./components/Nav";
+
+const App = () => (
+  <Router>
+    <div>
+      <Nav />
+			
+      <Switch>
+        <Route exact path="/" component={Items} />
+        <Route component={NoMatch} />
+      </Switch>
+    </div>
+  </Router>
+);
+
+export default App;
