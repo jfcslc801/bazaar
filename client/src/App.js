d080ab29d74a7a691e929577107ca6f659131e20
ctrlz <pehisaac@gmail.com>
2018-06-26 22:34:55 -0600

    Holy Shnikies Authentication can be a beezy.

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,93 @@
-import React from "react";
+import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Items from "./pages/Items";
-// import Books from "./pages/Books";
-// import Detail from "./pages/Detail";
 import NoMatch from "./pages/NoMatch";
 import Nav from "./components/Nav";
+import firebase from "firebase/app";
+import "firebase/auth";
 
-const App = () => (
-  <Router>
-    <div>
-      <Nav />
-			
-      <Switch>
-        <Route exact path="/" component={Items} />
-        <Route component={NoMatch} />
-      </Switch>
-    </div>
-  </Router>
-);
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyDbX869774z70nCO0tq_S2JRqLV_1118KM",
+  authDomain: "bazaar-260d7.firebaseapp.com",
+  databaseURL: "https://bazaar-260d7.firebaseio.com",
+  projectId: "bazaar-260d7",
+  storageBucket: "bazaar-260d7.appspot.com",
+  messagingSenderId: "188721019857"
+};
+firebase.initializeApp(config);
+// Variables with user authentication
+const auth = firebase.auth();
+auth.onAuthStateChanged(firebaseUser => { });
+
+class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+      isLoggedIn : false,
+      username : null
+    };
+    
+  }
+
+  signUp = (inputEmail, inputPassword) => {
+
+    // Sign up 
+    const promise = auth.createUserWithEmailAndPassword(inputEmail, inputPassword);
+    promise
+      .then(response => {
+        console.log('response', response);
+        this.setState({
+          username : response.user.email,
+          isLoggedIn : true
+        })
+      })
+      .catch(e => {
+        console.log(e.message)
+        // $("#errormessage").html(e.message);
+        console.log('you didnt sign in');
+        // run the error modal
+        // logInError();
+      });
+
+}
+
+logIn = (inputEmail, inputPassword) => {
+  
+    // Sign in 
+    const promise = auth.signInWithEmailAndPassword(inputEmail, inputPassword);
+    promise
+      .then(response => {
+        console.log('logged in', response);
+
+      })
+      .catch(e => {
+        console.log(e.message)
+        console.log('you didnt sign in');
+
+      });
+
+}
+
+  render() {
+    console.log(this);
+    
+    return (
+
+      <Router>
+        <div>
+          <Nav signUp={this.signUp} logIn={this.logIn} isLoggedIn={this.state.isLoggedIn} username={this.state.username} />
+
+          <Switch>
+          <Route path="/" render={()=> <Items signUp={this.signUp} />}/>
+            {/* <Route exact path="/" component={Items} /> */}
+            <Route component={NoMatch} />
+          </Switch>
+        </div>
+      </Router>
+    )
+  }
+
+};
 
 export default App;
--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -1,16 +1,45 @@
-import React from "react";
+import React, { Component } from 'react';
 import { Navbar, NavItem, Icon } from 'react-materialize';
+import LogIn from "./../../components/LogInModal"
 
-const Nav = () => (
+class Nav extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			
+		};
 
-    <Navbar brand='Bazaar' right>
-    <NavItem href='https://www.google.com'><Icon>search</Icon></NavItem>
-    <NavItem href='get-started.html'><i class="large material-icons">account_circle</i></NavItem>
-    <NavItem href='get-started.html'><i class="large material-icons">location_on</i></NavItem>
-  </Navbar>
+	}
+
+  clicky = (event) => {
+    // console.log('test');
+    
+    return event;
+  }
+
+
+    render () {
+      return (
+        <div>
+        <Navbar brand='Bazaar' right>
+          <LogIn signUp={this.props.signUp} logIn={this.props.logIn} />
+          {/* {!props.isLoggedIn ? 
+          <NavItem href='get-started.html'><i class="large material-icons">account_circle</i></NavItem>
+          :
+          <NavItem href='get-started.html'>{props.username}</NavItem>
+          } */}
+          <NavItem href='https://www.google.com'><Icon>search</Icon></NavItem>
+          <NavItem href='get-started.html'><i class="large material-icons">location_on</i></NavItem>
+        </Navbar>
+        
+        </div>
+      
+      )
+    }
 
 
 
-);
+}
+
 
 export default Nav;
