54217c50d544b46d5dd2b5ae555be77aab2eb99e
jmcam80 <jmcam80@gmail.com>
2018-07-04 16:11:08 -0600

    finished different models and their controllers

--- controllers/offersController.js
+++ controllers/offersController.js
@@ -1,35 +1,35 @@
 const db = require("../models");
 
-// Defining methods for the itemsController
+// Defining methods for the OffersController
 module.exports = {
   findAll: function (req, res) {
-	db.Item
+	db.Offer
 	    .find(req.query)
 	    .sort({ date: -1 })
 		.then(dbModel => res.json(dbModel))
 		.catch(err => res.status(422).json(err));
 	},
   findById: function(req, res) {
-    db.Item
+    db.Offer
       .findById(req.params.id)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
   create: function(req, res) {
 		console.log(req.body);
-    db.Item
+    db.Offer
       .create(req.body)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
   update: function(req, res) {
-    db.Item
+    db.Offer
       .findOneAndUpdate({ _id: req.params.id }, req.body)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
   remove: function(req, res) {
-    db.Item
+    db.Offer
       .findById({ _id: req.params.id })
       .then(dbModel => dbModel.remove())
       .then(dbModel => res.json(dbModel))
