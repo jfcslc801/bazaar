d5faa6e826a24d4d9ffb7b01a69c3d056a26b288
jaime franco cerritos <jfcslc@outlook.com>
2018-07-05 20:15:51 -0600

    modal

--- client/src/components/CardPanel/CardPanel.js
+++ client/src/components/CardPanel/CardPanel.js
@@ -1,7 +1,38 @@
 import React from "react";
 import "./CardPanel.css";
 import { CardPanel, Row, Col, Table } from 'react-materialize';
+// import "./ItemCard.css";
+import CustomModal from "./../../components/Modal";
+import { List, ListItem, Table1, TableItem } from './../../components/BarterWindow';
 
+let dbOffer = [{
+	id: 1,
+	date: "09/09/09",
+	offer: "iPhone SE 64GB",
+	itemDetails: "It is a great condition phone, AT&T Network",
+	user: "Izzlenizzle"
+},
+{
+	id: 2,
+	date: "09/09/09",
+	offer: "iPhone SE 64GB",
+	itemDetails: "It is a great condition phone, AT&T Network",
+	user: "Izzlenizzle"
+},
+{
+	id: 3,
+	date: "09/09/09",
+	offer: "iPhone SE 64GB",
+	itemDetails: "It is a great condition phone, AT&T Network",
+	user: "Izzlenizzle"
+},
+{
+	id: 4,
+	date: "09/09/09",
+	offer: "iPhone SE 64GB",
+	itemDetails: "It is a great condition phone, AT&T Network",
+	user: "Izzlenizzle"
+}]
 
 const CustomCardPanel = (props) => (
   <div>
@@ -12,6 +43,33 @@ const CustomCardPanel = (props) => (
             alt={props.name}
             src={props.image}
           />
+
+          <CustomModal>
+            {/* <List>
+						<ListItem
+							date={dbOffer.date}
+							offer={dbOffer.offer}
+							itemDetails={dbOffer.itemDetails}
+							user={dbOffer.user}
+						>
+						</ListItem>
+					</List> */}
+
+            <Table1>
+              {dbOffer.map(offer => (
+                <TableItem
+                  key={offer.id}
+                  user={offer.user}
+                  date={offer.date}
+                  offer={offer.offer}
+
+                />
+              ))}
+            </Table1>
+
+
+
+          </CustomModal>
           <p>{props.name}</p>
           <p>{props.value}</p>
           <p>{props.location}</p>
